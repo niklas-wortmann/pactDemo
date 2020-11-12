8d8a05b8b86e6d4629b060c9c7ef46e237948b60
Jan-Niklas Wortmann <jan-niklas.wortmann@evorait.com>
2020-11-11 19:22:33 -0600

    initial commit

---
+++ apps/heroes-consumer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pact-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'heroes-consumer';
+}
